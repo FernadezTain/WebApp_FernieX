d46ccd8b6aa9a08c809125608be666df595fd6a6
FernadezTain <fernadezofficial489@gmail.com>
2025-08-24 15:10:06 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
+const tg = window.Telegram.WebApp;
+tg.expand();
+
+const profileBtn = document.getElementById('profileBtn');
+const profileInfo = document.getElementById('profile-info');
+
+// Замените на свой домен и порт FastAPI
+const API_URL = "http://0.0.0.0:3925/profile/";
+
 profileBtn.addEventListener('click', async () => {
     const user_id = tg.initDataUnsafe.user.id;
     try {
-        const response = await fetch(`https://твой-домен:8000/profile/${user_id}`);
+        const response = await fetch(`${API_URL}${user_id}`);
         const data = await response.json();
         if(data.error){
             profileInfo.textContent = data.error;
